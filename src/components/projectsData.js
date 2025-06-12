4a405600262ac570453a071e8428f1eaaa6c332b
Nanakjot Singh Chahal <116296122+piroBeastie@users.noreply.github.com>
2025-06-12 15:34:56 +0530

    Update projectsData.js

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,57 +1,27 @@
 const projectsData=[
     {   
         key:1,
-        name:"Drake",
-        link:"https://www.google.com",
+        name:"NewsGenius",
+        link:"https://newgenius-frontend.vercel.app/",
         year:2025,
     },
     {
         key:2,
-        name:"Kanye West",
-        link:"https://www.google.com",
-        year:2025,
+        name:"Planet Dist",
+        link:"https://pirobeastie.github.io/Planet-Distance/",
+        year:2024,
     },
     {   
         key:3,
-        name:"Raju Punjabi",
-        link:"https://www.google.com",
+        name:"Spotify C",
+        link:"https://boisterous-mermaid-5baa81.netlify.app/",
         year:2025,
     },
     {   
-        key:1,
-        name:"Drake",
-        link:"https://www.google.com",
-        year:2025,
-    },
-    {
-        key:2,
-        name:"Kanye West",
-        link:"https://www.google.com",
-        year:2025,
-    },
-    {   
-        key:3,
-        name:"Raju Punjabi",
-        link:"https://www.google.com",
-        year:2025,
-    },
-    {   
-        key:1,
-        name:"Drake",
-        link:"https://www.google.com",
-        year:2025,
-    },
-    {
-        key:2,
-        name:"Kanye West",
-        link:"https://www.google.com",
-        year:2025,
-    },
-    {   
-        key:3,
-        name:"Raju Punjabi",
-        link:"https://www.google.com",
-        year:2025,
-    },
+        key:4,
+        name:"Retro CSS",
+        link:"https://pirobeastie.github.io/Job_Tracker_CSS-OG/",
+        year:2024,
+    }
 ]
 export default projectsData;
