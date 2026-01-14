cb351b0a6e6fe31dc875f1a28b7fcae1a04149e7
Nanakjot Singh Chahal <116296122+piroBeastie@users.noreply.github.com>
2026-01-14 19:56:40 +0530

    Updated projects data

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,24 +1,30 @@
 const projectsData=[
     {   
         key:1,
+        name:"Nile E-commerce",
+        link:"https://nile-theta.vercel.app",
+        year:2026,
+    },
+    {   
+        key:2,
         name:"NewsGenius",
         link:"https://newgenius-frontend.vercel.app/",
         year:2025,
     },
     {
-        key:2,
+        key:3,
         name:"Planet Dist",
         link:"https://pirobeastie.github.io/Planet-Distance/",
         year:2024,
     },
     {   
-        key:3,
+        key:4,
         name:"Spotify C",
         link:"https://boisterous-mermaid-5baa81.netlify.app/",
         year:2025,
     },
     {   
-        key:4,
+        key:5,
         name:"Retro CSS",
         link:"https://pirobeastie.github.io/Job_Tracker_CSS-OG/",
         year:2024,
